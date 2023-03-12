9230cc2cf28546a88adaaf6c89b36f6210b06117
Nara Gross <121043592+Fontenelegross@users.noreply.github.com>
2023-03-12 17:09:13 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,30 @@
+const botoescarrosel = document.querySelectorAll('.botao')
+const imagens = document.querySelectorAll('.imagem')
+botoescarrosel.forEach((botao, indice) => {
+    botao.addEventListener('click', () => {
+        desativarBotaoSelecionado()
+        selecionarBotaoCarrosel(botao)
+        esconderImagemAtiva() 
+        mostrarImagemDeFundo(indice)
+
+
+    })
+})
+
+function mostrarImagemDeFundo(indice) {
+    imagens[indice].classList.add('ativa')
+}
+
+function selecionarBotaoCarrosel(botao) {
+    botao.classList.add('selecionado')
+}
+
+function esconderImagemAtiva() {
+    const imagemativa = document.querySelector('.ativa')
+    imagemativa.classList.remove('ativa')
+}
+
+function desativarBotaoSelecionado() {
+    const botaoselecionado = document.querySelector('.selecionado')
+    botaoselecionado.classList.remove('selecionado')
+}
